f2368d39659b3c1352c8b303b27dbab45dfc60a9
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-06 21:39:09 +0530

    Navigation Bar Cunfigurations

---
+++ script.js
@@ -0,0 +1,9 @@
+// Search Button Activation
+
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = () => {
+    searchBtn.classList.toggle('fa-times');
+    searchForm.classList.toggle('active');
+}
